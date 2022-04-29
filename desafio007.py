97c059b13d1d5e62df4d0869216b9db5ccde0b15
Carlos Curty <carloscurty@hotmail.com>
2022-04-29 11:01:26 -0300

    Update ex 007 a 009

--- desafio007.py
+++ desafio007.py
@@ -4,6 +4,6 @@ print('Desafio 007: Mostrando a média de duas notas de um aluno')
 
 n1 = float(input('Primeira nota: '))
 n2 = float(input('Segunda nota: '))
-md = (n1 + n1) / 2
+md = (n1 + n2) / 2
 
 print('Sua média é: {:.2f}'.format(md))
--- desafio009.py
+++ desafio009.py
@@ -1,11 +1,11 @@
-# Desafio:
+# Desafio: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
 
 print('Desafio 009: Mostrando a tabuada de um número')
 
 num = int(input('Digite um número inteiro: '))
-print('Tabuada de {}'.format(num))
+print('Tabuada de {}:'.format(num))
 for x in range(1, 11):
-    if num*x < 9: m = ' '+str(num * x)
+    if (num * x) < 9: m = ' ' + str(num * x)
     else: m = num * x
     if x == 10:
         print('{}  X {}  = \t{}'.format(num, x, m))
