8bb5b0390946791ea751684769b006e02ac1ff8d
Carlos Curty <carloscurty@hotmail.com>
2022-05-06 19:39:09 -0300

    Update ex016.py

--- ex016.py
+++ ex016.py
@@ -1,5 +1,5 @@
 # Ex016: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
-import math
+from math import trunc
 print('Exercício 016: Quebrando um número')
 num = float(input('Digite um número: '))
-print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, math.trunc(num)))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))
